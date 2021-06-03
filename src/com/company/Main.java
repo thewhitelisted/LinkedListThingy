e02afcf938f147667e52cd3bed2c8c0b25434996
The whitelisted <53957527+thewhitelisted@users.noreply.github.com>
2021-06-03 08:16:48 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
     static LinkedList newLinkedList = new LinkedList(new Node(5));
     public static void main(String[] args) {
-	    newLinkedList.addValue(4);
-	    newLinkedList.addBeginValue(6);
+	// this code isn't needed just shows examples of linked list
+	newLinkedList.addValue(4);
+	newLinkedList.addBeginValue(6);
         newLinkedList.printList();
         newLinkedList.contains(4);
     }
